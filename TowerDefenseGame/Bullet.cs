c75254a30ee25b42014918285f4c896a243b6aff
simon6 <simonszeiler@gmail.com>
2020-04-18 09:47:13 +0200

    Creating bullet class

---
+++ TowerDefenseGame/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefenseGame
+{
+    class Bullet : Projectile
+    {
+        public Bullet(double x, double y, double w, double h) : base(x, y, w, h)
+        {
+
+        }
+    }
+}
