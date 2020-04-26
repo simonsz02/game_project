d0ccde9e5997ac89dfc380e4c2ecbb2bcb7237cc
László Deák <deaklaca@hotmail.com>
2020-04-26 17:00:32 +0200

    Game Menu implementation

---
+++ TowerDefenseGame/Menu/MenuModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefenseGame.Menu
+{
+    class MenuModel
+    {
+        public double GameWidth { get; set; }
+        public double GameHeight { get; set; }
+
+        public MenuModel(double gameWidth, double gameHeight)
+        {
+            GameWidth = gameWidth;
+            GameHeight = gameHeight;
+        }
+    }
+}
