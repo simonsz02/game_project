361026a2b1e63a06442301c70521edfbbd26f3b7
simon6 <simonszeiler@gmail.com>
2020-05-04 01:06:07 +0200

    Classes added

---
+++ TowerDefenseGame.Model/GameItems/GunTower.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace TowerDefenseGame.Model.GameItems
+{
+    public class GunTower : Tower
+    {
+        public int Price
+        {
+            get { return 200; }
+        }
+
+        public GunTower(double x, double y, double w, double h, Action<double, double, double, double, int, int, DamageType, Enemy> L, DispatcherTimer timer, DamageType dt = DamageType.physical) : base(x, y, w, h, L, timer, dt)
+        {
+
+        }
+    }
+}
---
+++ TowerDefenseGame.Model/GameItems/RocketTower.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace TowerDefenseGame.Model.GameItems
+{
+    public class RocketTower : Tower
+    {
+        public int Price
+        {
+            get { return 250; }
+        }
+
+        public RocketTower(double x, double y, double w, double h, Action<double, double, double, double, int, int, DamageType, Enemy> L, DispatcherTimer timer, DamageType dt = DamageType.physical) : base(x, y, w, h, L, timer, dt)
+        {
+        }
+    }
+}
