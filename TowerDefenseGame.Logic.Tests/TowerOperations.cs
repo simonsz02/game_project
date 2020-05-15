a4a1642cf10f881ecc574119f98cfdda42187632
simon6 <simonszeiler@gmail.com>
2020-05-15 23:13:15 +0200

    Test

---
+++ TowerDefenseGame.Logic.Tests/TowerOperations.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TowerDefenseGame.Model;
+
+namespace TowerDefenseGame.Logic.Tests
+{
+
+    [TestFixture]
+    public class TowerOperations
+    {
+
+
+        [TestCase]
+        public void AddTower()
+        {
+            //Arrange
+            TowerDefenseModel model = new TowerDefenseModel(800, 600, 1000);
+            TowerDefenseLogic logic = new TowerDefenseLogic(model);
+            bool OperationHasFailed;
+
+            //Act
+            OperationHasFailed = logic.AddOrUpgradeTower(new Point(1, 1), new System.Windows.Threading.DispatcherTimer());
+
+            //Assert
+            Assert.Equals(OperationHasFailed,false);
+
+        }
+    }
+}
